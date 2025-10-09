819cee9a3c0dd1b93e61761d88604284435e00b1
Oxyliteral <85361768+Oxyliteral@users.noreply.github.com>
2025-10-09 17:17:08 -0400

    Delete o_sspecial directory

--- o_sspecial/src/hullmods/HullMod_Unlock_Hull1.java
+++
@@ -1,7 +0,0 @@
-package hullmods;
-
-import com.fs.starfarer.api.combat.BaseHullMod;
-
-public class HullMod_Unlock_Hull1 extends BaseHullMod {
-
-}
--- o_sspecial/src/hullmods/HullMod_Unlock_Weapon1.java
+++
@@ -1,7 +0,0 @@
-package hullmods;
-
-import com.fs.starfarer.api.combat.BaseHullMod;
-
-public class HullMod_Unlock_Weapon1 extends BaseHullMod {
-
-}
